8202a0cb959b1434f28f07d574d2b38aaef213de
VictorJimenez99 <jimenezvictor99@gmail.com>
2021-06-21 08:15:47 -0500

    DOTNET Installation

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,13 +1,22 @@
 #!bin/bash
 
-CLANGD_PACKAGE=clangd-12
+UBUNTU_VERSION=20.04
 
-sudo apt install curl npm neovim vlc $(CLANGD_PACKAGE) fish libsqlite3-dev sqlite3 r-base -y
+CLANGD_PACKAGE=clangd-12
+JAVA_PACKAGE=openjdk-16-jdk
+
+DOTNET_INSTALLATION=true
+DOTNET_PACKAGE=dotnet-sdk-5.0
+DOTNET_RUNTIME_PACKAGE=aspnetcore-runtime-5.0
+
+
+sudo apt install curl npm neovim vlc $JAVA_PACKAGE $CLANGD_PACKAGE fish libsqlite3-dev sqlite3 r-base nvidia-cuda-toolkit maven -y
+
 
 sudo apt install libcurl4-openssl-dev libxml2-dev -y
 
 
-sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/$(CLANGD_PACKAGE) 100
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/$CLANGD_PACKAGE 100
 
 sudo snap install --classic intellij-idea-ultimate
 sudo snap install --classic clion
@@ -37,6 +46,22 @@ sudo Rscript ./initial_config.R
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 
+if ["$DOTNET_INSTALLATION" = true] 
+then
+    wget https://packages.microsoft.com/config/ubuntu/$UBUNTU_VERSION/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    sudo apt install apt-transport-https -y
+    sudo apt update
+    sudo apt install -y $DOTNET_PACKAGE
+    DOTNET_CLI_TELEMETRY_OPTOUT=1
+    sudo apt install -y $DOTNET_RUNTIME_PACKAGE
+    sudo apt install mono-devel
+    sudo apt install mono-complete
+    sudo snap install rider --clasic
+ 
+fi
+
+
 # After
 echo "READ BEFORE YOU GO"
 
@@ -44,6 +69,8 @@ echo "READ BEFORE YOU GO"
 echo "You still need to install rstudio download the latest version at"
 echo "https://www.rstudio.com/products/rstudio/download/#download"
 echo "and use sudo apt install to set everything up"
+
+echo "in order to fix jetbrains problem with fish sh you need to go to File -> Settings -> Tools -> Terminal -> (uncheck) Shell Integration"
 
 echo "Dont forget to update your nvidia drivers"
 
